fc4e54f980a242ae790d6da370e43cf09916e9d9
Mert Furkan Eraslan <mertfurkaneraslan@icloud.com>
2026-08-02 21:01:52 +0300

    Finalize drop sound timing

--- Assets/Scripts/BackgroundMusicController.cs
+++ Assets/Scripts/BackgroundMusicController.cs
@@ -14,8 +14,8 @@ namespace BottleBattle
         private const string MusicMutedKey = "BottleBattle.MusicMuted";
         private const float DefaultVolume = 0.23f;
         private const float DropSoundVolume = 0.7f;
-        private const float DropSoundStartTime = 0.37f;
-        private const float DropSoundDuration = 0.23f;
+        private const float DropSoundStartTime = 0.36f;
+        private const float DropSoundDuration = 0.24f;
 
         private static BackgroundMusicController instance;
 
